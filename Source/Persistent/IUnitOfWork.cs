b49d9a2a749ce75ecea1bad5911771cfa2609789
Alex <alexcoh19@outlook.com>
2026-08-31 03:06:01 +0200

    feat: adds unitofwork pattern serving as abstract layer between service and repository
    part of the prev commit which updated the repository pattern

---
+++ Source/Persistent/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+using Source.Repositories.IRepositories;
+
+namespace Source.Persistent
+{
+	/// <summary>
+	///		Serves as a layer of abstraction between Service and the Repository layer.
+	/// </summary>
+	public interface IUnitOfWork : IDisposable
+	{
+		IAdministratorRepository Administrators { get; }
+		IEmployeeRepository Employees { get; }
+		IHumanRepository Humans { get; }
+		IPrincipalRepository Principals { get; }
+		IStudentRepository Students { get; }
+		ITeacherRepository Teachers { get; }
+
+		/// <summary>
+		///		Invokes save changes.
+		/// </summary>
+		/// <returns>The number of rows affected by the database operation.</returns>
+		int Save();
+		/// <summary>
+		///		Invokes save 
+		/// </summary>
+		/// <returns>The number of rows affected by the database operation.</returns>
+		Task<int> SaveAsync();
+	}
+}
