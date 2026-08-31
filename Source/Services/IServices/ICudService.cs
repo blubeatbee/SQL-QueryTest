257aa5d6c251b9d10960dc7fe20840811edb3cb7
Alex <alexcoh19@outlook.com>
2026-08-31 10:08:55 +0200

    feat: will remove icudservice.cs

--- Source/Services/IServices/ICudService.cs
+++
@@ -1,9 +0,0 @@
-namespace Source.Services.IServices
-{
-	public interface ICudService<T1, T2>
-	{
-		void CreateOneEntry(T2 newEntryDto);
-		//void UpdateOneEntry(T1 id, T2 updateEntryDto);
-		//void DeleteOneEntry(T1 id);
-	}
-}
